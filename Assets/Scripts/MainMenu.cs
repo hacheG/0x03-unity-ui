521c831d75e5106cc870499d275eb7b87957be1e
HacheG <943@holbertonschool.com>
2020-05-21 17:37:37 -0500

    task

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,9 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,16 @@ public class MainMenu : MonoBehaviour
     public void PlayMaze()
     {
         SceneManager.LoadScene(1);
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
     }
 
     public void QuitMaze()
